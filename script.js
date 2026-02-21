ea51ac47aa9c762d5d2af670c9bae06df12c0fe9
Tormyze <paulojroliver@gmail.com>
2026-02-21 10:38:24 -0300

    Carousel and animations implemented

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector("#slogan").classList.add("visible")
+const carouselBox = document.querySelector('#carousel_box')
+
+// código base para o Observer
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((e) => {
+        if (e.isIntersecting) {
+            e.target.classList.add('visible')
+            
+            observer.unobserve(e.target)
+        }
+    })
+}, {
+    threshold: 0.5 // 50% do elemento visível
+})
+
+observer.observe(carouselBox);
